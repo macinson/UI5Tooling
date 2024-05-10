bed2cb90ae03552e3301e4322dcc8ce06c7ed427
Maciej <M.Kozik@student.tudelft.nl>
2024-05-10 20:26:43 +0200

    JSON model added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/Text'
-], (Controller,Text) => {
+    'sap/m/Text',
+    "sap/ui/model/json/JSONModel"
+], (Controller,Text, JSONModel) => {
     'use strict';
     
     return Controller.extend(
         "ui5.tooling.view.App",{
+            onInit(){
+                const oData = {
+                    test:{
+                        ff: "test"
+                    }
+                };
+                
+                const oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+            },
             buttonClicked(){
                 alert("Clicked");
             },
