82eccf88c31f1a3a917a96e2607ada1677621940
Maciej Kozik <M.Kozik@student.tudelft.nl>
2024-06-16 14:43:18 +0200

    Adding invoices (without inputs for now)

--- webapp/controller/AddInvoice.controller.js
+++ webapp/controller/AddInvoice.controller.js
@@ -3,5 +3,18 @@ sap.ui.define([
 ], (Controller) => {
     'use strict';
     
-    return Controller.extend("ui5.tooling.controller.AddInvoice",{})
+    return Controller.extend("ui5.tooling.controller.AddInvoice",{
+        saveInvoice(){
+            var model = this.getView().getModel("invoice")
+            var productName = this.getView().byId("productName")
+            model.getData().Invoices.push({"ProductName": "Pineapple",
+            "Quantity": 21,
+            "ExtendedPrice": 87.2,
+            "InvoiceDate": "2015-04-01T00:00:00",
+            "InvoiceNumber": "A"})
+            model.updateBindings()
+            var oRouter = this.getOwnerComponent().getRouter()
+            oRouter.navTo("overview")
+        }
+    })
 });
