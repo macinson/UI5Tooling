2cbd7848dab5cd5839f4aa161d71fe31ba986df2
Maciej Kozik <M.Kozik@student.tudelft.nl>
2024-06-16 14:47:00 +0200

    Minor change

--- webapp/controller/AddInvoice.controller.js
+++ webapp/controller/AddInvoice.controller.js
@@ -7,11 +7,12 @@ sap.ui.define([
         saveInvoice(){
             var model = this.getView().getModel("invoice")
             var productName = this.getView().byId("productName")
-            model.getData().Invoices.push({"ProductName": productName.getValue(),
-            "Quantity": 21,
-            "ExtendedPrice": 87.2,
-            "InvoiceDate": "2015-04-01T00:00:00",
-            "InvoiceNumber": "A"})
+            model.getData().Invoices.push({
+                "ProductName": productName.getValue(),
+                "Quantity": 21,
+                "ExtendedPrice": 87.2,
+                "InvoiceDate": "2015-04-01T00:00:00",
+                "InvoiceNumber": "A"})
             model.updateBindings()
             var oRouter = this.getOwnerComponent().getRouter()
             oRouter.navTo("overview")
